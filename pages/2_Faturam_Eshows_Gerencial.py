97621e0d146acce408de7b3e3124d633eceebe39
gabrielcunha91 <128188960+gabrielcunha91@users.noreply.github.com>
2024-04-25 16:49:58 +0000

    msg

--- pages/2_Faturam_Eshows_Gerencial.py
+++ pages/2_Faturam_Eshows_Gerencial.py
@@ -17,9 +17,19 @@ df_view_faturam_eshows
 
 
 ### Agrupamentos ###
-df_view_faturam_ajustado = df_view_faturam_eshows[["Primeiro_Dia_Mes", "Casa", "Valor_Total"]]
+df_view_faturam_ajustado = df_view_faturam_eshows[["Primeiro_Dia_Mes", "p_ID", "Casa", "Valor_Total", "Comissao_Eshows_B2B",
+                                                    "Comissao_Eshows_B2C", "SAAS_Mensalidade", "SAAS_Percentual", "Curadoria",
+                                                      "Taxa_Adiantamento", "Taxa_Emissao_NF"]]
 
-df_view_faturam_por_mes =df_view_faturam_ajustado.groupby("Primeiro_Dia_Mes").agg({"Casa": "nunique", "Valor_Total": "sum"})
+df_view_faturam_por_mes = df_view_faturam_ajustado.groupby("Primeiro_Dia_Mes").agg(
+    {"Casa": "nunique", "p_ID": "nunique", "Valor_Total": "sum", "Comissao_Eshows_B2B": "sum", "Comissao_Eshows_B2C": "sum",
+     "SAAS_Mensalidade": "sum", "SAAS_Percentual": "sum", "Curadoria": "sum", "Taxa_Adiantamento": "sum", "Taxa_Emissao_NF": "sum"})
+
+df_view_faturam_por_mes["Perc_Comissao"] = df_view_faturam_por_mes["Comissao_Eshows_B2B"]/df_view_faturam_por_mes["Valor_Total"]
+colunas_para_somar_faturam = ["Comissao_Eshows_B2B", "Comissao_Eshows_B2C", "SAAS_Mensalidade", "SAAS_Percentual", "Curadoria",
+                              "Taxa_Adiantamento", "Taxa_Emissao_NF"]
+
+df_view_faturam_por_mes["Faturam_Total"] = df_view_faturam_por_mes[colunas_para_somar_faturam].sum(axis=1)
 
 df_view_faturam_por_mes
 
